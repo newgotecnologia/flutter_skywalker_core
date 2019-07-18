3fabda8ed3a812aa5a50ec9c16ffea8a3846fb4e
Marcelo Vilas Boas Correa Filho <marcelovbcfilho@gmail.com>
2019-07-18 15:18:24 -0300

    feat: SkySwitch created Created a component to use Switch in flutter

---
+++ lib/src/widgets/sky_switch.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class SkySwitch extends StatefulWidget {
+  final bool isSwitched;
+  final Color activeTrackColor;
+  final Color activeColor;
+
+  const SkySwitch(
+      {Key key, this.isSwitched, this.activeTrackColor, this.activeColor})
+      : super(key: key);
+
+  @override
+  _SkySwitchState createState() =>
+      _SkySwitchState(isSwitched, activeTrackColor, activeColor);
+}
+
+class _SkySwitchState extends State<SkySwitch> {
+  bool isSwitched;
+  Color activeTrackColor;
+  Color activeColor;
+
+  _SkySwitchState(this.isSwitched, this.activeTrackColor, this.activeColor);
+
+  @override
+  Widget build(BuildContext context) {
+    return //this goes in our State class as a global variable
+
+//this goes in as one of the children in our column
+        PlatformSwitch(
+      value: isSwitched,
+      onChanged: (value) {
+        setState(() {
+          isSwitched = value;
+        });
+      },
+      android: (context) {
+        return MaterialSwitchData(activeTrackColor: activeTrackColor);
+      },
+      activeColor: Colors.green,
+    );
+  }
+}
