9e4bff9753616f51d42e5c0aad583152a51ade1f
Yehoshua Oliveira <ioxua.oliveira@gmail.com>
2019-07-18 17:05:22 -0300

    feat: adds export directives to lib index

--- lib/flutter_skywalker_core.dart
+++ lib/flutter_skywalker_core.dart
@@ -1,7 +1,10 @@
 library flutter_skywalker_core;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:flutter_skywalker_core/src/widgets/sky_button.dart';
+export 'package:flutter_skywalker_core/src/widgets/sky_dropdown_button.dart';
+export 'package:flutter_skywalker_core/src/widgets/sky_flat_button.dart';
+export 'package:flutter_skywalker_core/src/widgets/sky_modal_popup.dart';
+export 'package:flutter_skywalker_core/src/widgets/sky_switch.dart';
+export 'package:flutter_skywalker_core/src/widgets/sky_text.dart';
+export 'package:flutter_skywalker_core/src/widgets/sky_text_button.dart';
+export 'package:flutter_skywalker_core/src/widgets/sky_text_field.dart';
