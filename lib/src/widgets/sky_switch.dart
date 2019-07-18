19a0598ad8b2a06016e300dbaa120f22dfe1edf4
marcelovbcfilho <51462138+marcelovbcfilho@users.noreply.github.com>
2019-07-18 17:02:38 -0300

    merge: some required adjustments to components
    * fix: SkyTextField
    Now can send a single text field or a label with text field
    
    * fix: SkyTextField
    Now the text fields are inside a FitedBox to adpt to the container's sizes
    
    * fix: SkyTextField
    Now the SkytextField can receive size to adjust to the container
    
    * fix: SkyTextField
    Now the SkytextField can receive size to adjust to the container
    
    * fix: SkyTextField was crashing
    
    * fix: DropDown conatiner's
    
    * fix: SkyDropdown
    
    * fix: SkyDropdownButton
    
    * fix: SkyDropdownButton
    
    * fix: SkyText no longer return a PlatformText
    
    * feat: SkySwitch created
    Created a component to use Switch in flutter
    
    * fix: SkySwitch
    SkySwitch now has a defined size, so when turn horizontaly the phone it wont be derformed
    
    * fix: SkySwitch
    SkySwitch now has a default color so don't crash

---
+++ lib/src/widgets/sky_switch.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class SkySwitch extends StatefulWidget {
+  final bool isSwitched;
+  final Color activeTrackColor;
+  final Color activeColor;
+
+  const SkySwitch(
+      {Key key, this.isSwitched = false, this.activeTrackColor = Colors.lightBlue, this.activeColor = Colors.blue})
+      : super(key: key);
+
+  @override
+  _SkySwitchState createState() =>
+      _SkySwitchState(isSwitched, activeTrackColor, activeColor);
+}
+
+class _SkySwitchState extends State<SkySwitch> {
+  bool isSwitched;
+  Color activeTrackColor;
+  Color activeColor;
+
+  _SkySwitchState(this.isSwitched, this.activeTrackColor, this.activeColor);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      child: PlatformSwitch(
+        value: isSwitched,
+        onChanged: (value) {
+          setState(() {
+            isSwitched = value;
+          });
+        },
+        android: (context) {
+          return MaterialSwitchData(activeTrackColor: activeTrackColor);
+        },
+        activeColor: activeColor,
+      ),
+    );
+  }
+}
