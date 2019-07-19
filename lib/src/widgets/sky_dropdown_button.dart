f60ec3864e973f895d65a2bafebcb5bab1958ef9
Yehoshua Oliveira <ioxua.oliveira@gmail.com>
2019-07-19 18:06:09 -0300

    refactor: generalizes SkyDropdownButton

--- lib/src/widgets/sky_dropdown_button.dart
+++ lib/src/widgets/sky_dropdown_button.dart
@@ -1,22 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_skywalker_core/src/widgets/sky_text.dart';
 
-class SkyDropdownButton extends StatefulWidget {
-  final List<String> items;
+typedef String OptionTitleResolver<T>(T t);
+
+class SkyDropdownButton<T> extends StatefulWidget {
+  final List<T> items;
   final Color textColor;
   final Color dropdownIconColor;
   final double fontSize;
+  final OptionTitleResolver<T> resolver;
 
   const SkyDropdownButton({
     Key key,
     this.items,
+    this.resolver,
     this.textColor = Colors.black,
     this.dropdownIconColor = Colors.blue,
     this.fontSize = 15,
   }) : super(key: key);
 
+  static SkyDropdownButton<bool> yesOrNo({
+    yesTitle = "Yes",
+    noTitle = "No",
+    textColor = Colors.black,
+    dropdownIconColor = Colors.blue,
+    fontSize = 15,
+  }) {
+    return SkyDropdownButton<bool>(
+      textColor: textColor,
+      dropdownIconColor: dropdownIconColor,
+      fontSize: fontSize,
+      items: [true, false],
+      resolver: (bool val) {
+        if (val) {
+          return yesTitle;
+        }
+        return noTitle;
+      },
+    );
+  }
+
   @override
-  _SkyDropdownButtonState createState() => _SkyDropdownButtonState(
+  _SkyDropdownButtonState createState() => _SkyDropdownButtonState<T>(
         items: items,
         textColor: textColor,
         fontSize: fontSize,
@@ -24,20 +49,27 @@ class SkyDropdownButton extends StatefulWidget {
       );
 }
 
-class _SkyDropdownButtonState extends State<SkyDropdownButton> {
-  final List items;
+class _SkyDropdownButtonState<T> extends State<SkyDropdownButton<T>> {
+  final List<T> items;
   final Color textColor;
   final Color dropdownIconColor;
   final double fontSize;
-  List<DropdownMenuItem<String>> _dropDownMenuItems;
-  String _currentItem;
+  final OptionTitleResolver<T> resolver;
 
-  _SkyDropdownButtonState(
-      {this.items, this.textColor, this.fontSize, this.dropdownIconColor});
+  List<DropdownMenuItem<T>> _dropDownMenuItems;
+  T _currentItem;
+
+  _SkyDropdownButtonState({
+    this.items,
+    this.textColor,
+    this.fontSize,
+    this.dropdownIconColor,
+    this.resolver,
+  });
 
   @override
   void initState() {
-    _dropDownMenuItems = getDropDownMenuItems();
+    _dropDownMenuItems = _buildDropdownItems();
     _currentItem = _dropDownMenuItems[0].value;
     super.initState();
   }
@@ -45,9 +77,10 @@ class _SkyDropdownButtonState extends State<SkyDropdownButton> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-        child: FittedBox(
-          fit: BoxFit.cover,
+      padding: EdgeInsets.all(0),
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: DropdownButtonHideUnderline(
           child: DropdownButton(
             items: _dropDownMenuItems,
             value: _currentItem,
@@ -59,26 +92,29 @@ class _SkyDropdownButtonState extends State<SkyDropdownButton> {
             style: TextStyle(fontSize: fontSize, color: textColor),
             isDense: true,
           ),
-        ));
+        ),
+      ),
+    );
   }
 
-  List<DropdownMenuItem<String>> getDropDownMenuItems() {
-    List<DropdownMenuItem<String>> items = List();
-    for (String item in this.items) {
+  List<DropdownMenuItem<T>> _buildDropdownItems() {
+    List<DropdownMenuItem<T>> items = List();
+    for (T item in this.items) {
       // here we are creating the drop down menu items, you can customize the item right here
       // but I'll just use a simple text for this
       items.add(DropdownMenuItem(
-          value: item,
-          child: SkyText(
-            item,
-            textColor: textColor,
-            fontSize: fontSize,
-          )));
+        value: item,
+        child: SkyText(
+          resolver(item),
+          textColor: textColor,
+          fontSize: fontSize,
+        ),
+      ));
     }
     return items;
   }
 
-  void changedDropDownItem(String selectedOption) {
+  void changedDropDownItem(T selectedOption) {
     setState(() {
       _currentItem = selectedOption;
     });
